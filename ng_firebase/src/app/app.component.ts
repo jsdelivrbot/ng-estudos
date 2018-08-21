a740a70a17a0994942f1a0ceccc7efc3e93621fd
LYNDON-PC\LYNDON-PC <integraldominio@gmail.com>
2018-08-21 11:50:24 -0300

    Teste com firebase.

--- ng_firebase/src/app/app.component.ts
+++ ng_firebase/src/app/app.component.ts
@@ -16,21 +16,17 @@ export class AppComponent {
   menu: Observable<Menu[]>;
   private iMenu: AngularFirestoreCollection<Menu>;
 
+  menu2: Array<Menu>;
+
   constructor(
     private db: AngularFireDatabase,
     private fire: AngularFireAuth) {
-    //  this.fire.auth.signInWithEmailAndPassword('integraldominio@gmail.com', '010203')
-    //  .then(user => {
-    //    this.fire.authState.subscribe( _auth => {
-    //      if (_auth) {
-    //        console.log('>>>logou');
-            this.menu = db.list<Menu>('cardapio').valueChanges();
-
-    //      }
-    //  });
-
-    // });
-   }
+    db.list<Menu>('menu')
+      .valueChanges()
+      .subscribe( (data) => {
+          this.menu2 = data;
+       });
+  }
 
   login() {
     this.fire.auth.signInWithPopup(new auth.GoogleAuthProvider());
@@ -65,28 +61,64 @@ export interface Menu {
 
 /*
 https://jsoneditoronline.org/
+
 {
-  "email":"demo@gmail.com",
-  "password":"010203",
-  "firstName":"demo",
-  "lastName":"demo",
-  "phone":"+55-45-9998200413",
-  "estabelecimento":
-    {
-    "nome":"BigBurger lanches",
-    "endereco":"Rua Osvaldo Goch,1190",
-    "cidade":"Foz Iguaçu",
-    "pais":"Brazil"
-    },
-  "horario":{
-    "seg-sex":{"ini":9,"fim":22},
-    "sab-dom":{"ini":9,"fim":22}
-  } ,
-  "mapa": {"lat": -32.364, "lng": 153.207},
-  "cardapio" :
-  [
-    {"nome":"BigBurger","texto":"Super Hambuger de carne com quiejo e salada.","preco":20, "ativo":true, "fotos":["foto"]} ,
-    {"nome":"Coca 500ml","texto":"coca-cola 500ml","preco":10,"ativo":true, "fotos":["foto"]}
-  ]
+   "menu" : [
+   { "email":"demo1@gmail.com",
+   "password":"010203",
+   "firstName":"demo",
+   "lastName":"demo",
+   "phone":"+55-45-9998200413",
+   "estabelecimento":
+     {
+     "nome":"BigBurger lanches",
+     "endereco":"Rua Osvaldo Goch,1190",
+     "cidade":"Foz Iguaçu",
+     "pais":"Brazil"
+     },
+   "horario":{
+     "seg-sex":{"ini":9,"fim":22},
+     "sab-dom":{"ini":9,"fim":22}
+   } ,
+   "mapa": {"lat": -32.364, "lng": 153.207},
+   "cardapio" :
+   [
+     {"nome":"BigBurger","texto":"Super Hambuger de carne com quiejo e salada.","preco":20, "ativo":true, "fotos":["foto"]} ,
+     {"nome":"Coca 500ml","texto":"coca-cola 500ml","preco":10,"ativo":true, "fotos":["foto"]}
+   ]
+   } ,
+   { "email":"demo2@gmail.com",
+   "password":"010203",
+   "firstName":"demo",
+   "lastName":"demo",
+   "phone":"+55-45-9998200413",
+   "estabelecimento":
+     {
+     "nome":"BigBurger lanches",
+     "endereco":"Rua Osvaldo Goch,1190",
+     "cidade":"Foz Iguaçu",
+     "pais":"Brazil"
+     },
+   "horario":{
+     "seg-sex":{"ini":9,"fim":22},
+     "sab-dom":{"ini":9,"fim":22}
+   } ,
+   "mapa": {"lat": -32.364, "lng": 153.207},
+   "cardapio" :
+   [
+     {"nome":"BigBurger","texto":"Super Hambuger de carne com quiejo e salada.","preco":20, "ativo":true, "fotos":["foto"]} ,
+     {"nome":"Coca 500ml","texto":"coca-cola 500ml","preco":10,"ativo":true, "fotos":["foto"]}
+   ]
+   } ]
 }
 */
+
+    //  this.fire.auth.signInWithEmailAndPassword('integraldominio@gmail.com', '010203')
+    //  .then(user => {
+    //    this.fire.authState.subscribe( _auth => {
+    //      if (_auth) {
+    //        console.log('>>>logou');
+    //      }
+    //  });
+    // });
+
