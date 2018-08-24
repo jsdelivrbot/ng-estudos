33434015b718f3e337bbc129db2fa9f861a7dda7
LYNDON-PC\LYNDON-PC <integraldominio@gmail.com>
2018-08-24 14:14:09 -0300

    set env.

--- ng_firebase_movie/src/environments/environment.prod.ts
+++ ng_firebase_movie/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
+    apiKey: 'xxx',
+    authDomain: 'jsa-angular6.firebaseapp.com',
+    databaseURL: 'https://jsa-angular6.firebaseio.com',
+    projectId: 'jsa-angular6',
     storageBucket: '',
-    messagingSenderId: ''
+    messagingSenderId: 'xxx'
   }
 };
